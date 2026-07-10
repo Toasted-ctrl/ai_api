1be120e9c166bfa813290825545ddb65e59430b6
Mick <themickbb@gmail.com>
2026-07-10 15:44:29 +0200

    Added uvicorn import.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 
 from api.v1 import root, servers, models, status, translation, llms
 from core.config import config
@@ -16,3 +17,9 @@ app.include_router(models.router, prefix=v1_prefix)
 app.include_router(status.router, prefix=v1_prefix)
 app.include_router(translation.router, prefix=v1_prefix)
 app.include_router(llms.router, prefix=v1_prefix)
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app=app,
+        host="0.0.0.0",
+        port=8000)
