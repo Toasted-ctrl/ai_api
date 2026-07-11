afc1d9d9e67e38aa9cd9648e0e55c46f216ad6ff
Mick <themickbb@gmail.com>
2026-07-11 20:24:47 +0200

    Reworked servers status and wake methods.

--- src/api/v1/servers.py
+++ src/api/v1/servers.py
@@ -1,83 +1,57 @@
-import requests
-
 from fastapi import APIRouter, HTTPException, status
 
 from core.config import config
-from models.m_servers import ReturnWakeServer, ReturnServerStatus, ReturnServers
-from servers.boot_server import boot_server
-from servers.status import is_server_online
+from io_models.servers import ResponseLocalServers, ResponseWakeServer
+from servers.boot_server import wake_server
+from servers.status import is_llm_available, is_server_online
 
 router = APIRouter()
-tags = ["Servers"]
 
 @router.get(
-    "/servers",
-    tags=tags,
-    response_model=ReturnServers
+    "/servers/on_prem",
+    tags=["Servers (On Prem)"],
+    response_model=ResponseLocalServers
 )
-def list_servers():
-    servers: dict = config.get_server_configuration
+def get_local_servers():
+    servers = []
+    for server_name in config.LOCAL_SERVER_CONFIGURATION.keys():
+        server = {}
+        server['server_name'] = server_name
+        server['online'] = is_server_online(
+            host=config.LOCAL_SERVER_CONFIGURATION[server_name]['hostname']
+        )
+        server['available'] = is_llm_available(
+            url=config.LOCAL_SERVER_CONFIGURATION[server_name]['base_url']
+        )
+        servers.append(server)
+
     return {
-        "detail": "Success",
-        "servers": [key for key in servers.keys()]
+        "on_prem": servers
     }
 
+
 @router.get(
-    "/servers/{server_name}/wake",
-    tags=tags,
-    response_model=ReturnWakeServer
+    "/servers/on_prem/wake/{server_name}",
+    tags=["Servers (On Prem)"],
+    response_model=ResponseWakeServer
 )
-def wake_server(server_name: str):
+def wake_local_server(server_name: str):
     try:
-        servers: dict = config.get_server_configuration
+        servers: dict = config.LOCAL_SERVER_CONFIGURATION
         if server_name not in servers.keys():
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
-                detail=f"Server not found: {server_name}"
+                detail=f"Server non-existant: {server_name}"
             )
         
         server: dict = servers.get(server_name)
-        boot_server(mac_address=server.get('mac_address'))
+        wake_server(mac_address=server.get('mac_address'))
         return {
-            "detail": f"Sent magic packet to '{server_name}' server. Please verify the server is online."
+            "detail": f"Magic packet sent to '{server_name}'. Please verify its status."
         }
     
     except KeyError:
         raise HTTPException(
             status_code=status.HTTP_501_NOT_IMPLEMENTED,
             detail=f"MAC Address not configured for server: {server_name}"
-        )
-
-@router.get(
-    "/servers/{server_name}/status",
-    tags=tags,
-    response_model=ReturnServerStatus
-)
-def get_server_status(server_name: str):
-    try:
-        servers: dict = config.get_server_configuration
-        if server_name not in servers.keys():
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail=f"Server not found: {server_name}"
-            )
-    
-        server: dict = servers.get(server_name)
-        server_status: bool = is_server_online(url=server.get('base_url'))
-        return {
-            "server_name": server_name,
-            "status": "Online",
-            "available": server_status 
-        }
-
-    except requests.exceptions.ConnectionError:
-        raise HTTPException(
-            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
-            detail=f"Server offline: {server_name}"
-        )
-    
-    except KeyError:
-        raise HTTPException(
-            status_code=status.HTTP_501_NOT_IMPLEMENTED,
-            detail=f"Base URL not configured for server: {server_name}"
         )
