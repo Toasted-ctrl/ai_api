398a5ab248a64981443d2a057d839cfd9f58c107
Mick <themickbb@gmail.com>
2026-07-11 20:26:32 +0200

    Added is_server_online and reworked is_llm_available.

--- src/servers/status.py
+++ src/servers/status.py
@@ -1,10 +1,29 @@
+import platform
 import requests
+import subprocess
 
-def is_server_online(url) -> bool:
+def is_server_online(host: str) -> bool:
+    
+    """Returns True if the returns a response. Requires pinging IP address or hostname."""
 
-    """Checking if the Ollama server is online at the specified address."""
+    # -n for Windows, -c for Unix
+    param = "-n" if platform.system().lower() == "Windows" else "-c"
+    result = subprocess.run(
+        ["ping", param, "1", host]
+    )
 
-    response = requests.get(url=url)
-    if not response.status_code == 200:
+    # Return True if online
+    return result.returncode == 0 
+
+
+def is_llm_available(url) -> bool:
+
+    """Returns True if the LLM service (e.g., Ollama) is available."""
+
+    try:
+        response = requests.get(url=url)
+        if not response.status_code == 200:
+            return False
+        return True
+    except Exception:
         return False
-    return True
