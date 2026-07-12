dabed9b57d52b503e9c5846dfd3cb7dd3320d929
Mick <themickbb@gmail.com>
2026-07-12 16:42:35 +0200

    Refactoring of translation endpoints.

--- src/api/v1/translation.py
+++ src/api/v1/translation.py
@@ -1,53 +1,66 @@
 from fastapi import APIRouter, HTTPException, status
 
-from core.config import config
-from io_models.translations import ResponseTranslation, PostTranslation
-from providers.ollama.translategemma import supported_languages as translategemma_languages
-from providers.ollama.translations import get_translation_translategemma
+from io_models.translations import (
+    ResponseTranslation,
+    PayloadTranslation,
+    ResponseTranslationLanguages
+)
+from providers.ollama.translategemma import (
+    translategemma_locate,
+    translategemma_languages,
+    get_translation_translategemma
+)
 
 router = APIRouter()
-tags = ["Translations"]
+tags = ["Translation"]
 
 @router.post(
-    "/models/translations",
+    "/translation/translategemma",
     tags=tags,
     response_model=ResponseTranslation
 )
-def post_translation(payload: PostTranslation):
+def post_translation_translategemma(payload: PayloadTranslation):
 
-    if payload.provider not in config.SUPPORTED_PROVIDERS:
+    if payload.from_lang_code not in translategemma_languages():
         raise HTTPException(
-            status_code=status.HTTP_406_NOT_ACCEPTABLE,
-            detail=f"Unsupported Provider: '{payload.provider}'"
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Unsupported language: {payload.from_lang_code}"
         )
     
-    if payload.provider == 'Ollama-1':
-
-        if payload.model == 'translategemma:latest':
-
-            supported_languages = translategemma_languages()
-            if payload.to_lang_code not in supported_languages:
-                raise HTTPException(
-                    status_code=status.HTTP_400_BAD_REQUEST,
-                    detail=f"Unsupported language: '{payload.to_lang_code}'"
-                )
-            if payload.from_lang_code not in supported_languages:
-                raise HTTPException(
-                    status_code=status.HTTP_400_BAD_REQUEST,
-                    detail=f"Unsupported language: '{payload.from_lang_code}'"
-                )
-
-            translation = get_translation_translategemma(
-                from_lang=payload.from_lang_code,
-                to_lang=payload.to_lang_code,
-                prompt=payload.prompt,
-                temperature=payload.parameters.temperature
-            )
-
-            return translation
+    if payload.to_lang_code not in translategemma_languages():
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Unsupported language: {payload.to_lang_code}"
+        )
     
-        else:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail=f"Unsupported Model: '{payload.model}'"
-            )
+    if payload.prompt == "":
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Prompt must not be None"
+        )
+
+    host = translategemma_locate()
+    if host is None:
+        raise HTTPException(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            detail="Translategemma is unavailable"
+        )
+    
+    return get_translation_translategemma(
+        from_lang=payload.from_lang_code,
+        to_lang=payload.to_lang_code,
+        prompt=payload.prompt,
+        temperature=payload.parameters.temperature,
+        host=host
+    )
+
+
+@router.get(
+    "/translation/translategemma",
+    tags=["Translation"],
+    response_model=ResponseTranslationLanguages
+)
+def get_languages_translategemma():
+    return {
+        "languages": translategemma_languages()
+    }
--- src/providers/ollama/translategemma.py
+++ src/providers/ollama/translategemma.py
@@ -1,6 +1,10 @@
 from functools import lru_cache
+from langchain_ollama import ChatOllama
 import json
 import os
+
+from core.config import config
+from providers.ollama.general import get_all_models_ollama
 
 @lru_cache(maxsize=1)
 def _load_translategemma_data() -> dict:
@@ -14,7 +18,7 @@ def _load_translategemma_data() -> dict:
         return json.load(file)
     
 
-def supported_languages() -> list[str]:
+def translategemma_languages() -> list[str]:
 
     """Returns a list of languages supported by Translategemma."""
 
@@ -38,3 +42,51 @@ def get_language(lang_code: str) -> str:
 
     data = _load_translategemma_data()
     return data['supported'][lang_code]
+
+
+def translategemma_locate() -> str:
+
+    """Returns the server on which translategemma is currently hosted."""
+
+    for provider in config.LOCAL_SERVER_CONFIGURATION.keys():
+        if 'translategemma:latest' in get_all_models_ollama(host_url=config.LOCAL_SERVER_CONFIGURATION[provider]['base_url'])['translation']:
+            return provider
+    return None
+
+
+def get_translation_translategemma(
+    from_lang: str,
+    to_lang: str,
+    prompt: str,
+    temperature: float,
+    host: str
+) -> dict:
+
+    """Invokes a translation from translategemma 4B"""
+
+    llm = ChatOllama(
+        model="translategemma:latest",
+        temperature=temperature,
+        base_url=config.LOCAL_SERVER_CONFIGURATION[host]['base_url'],
+        disable_streaming=True
+    )
+
+    from_language = get_language(from_lang)
+    to_language = get_language(to_lang)
+    from_lang_code = get_language_code(from_lang)
+    to_lang_code = get_language_code(to_lang)
+
+    instruction = f"""
+    You are a professional {from_language} ({from_lang_code}) to {to_language} ({to_lang_code}) translator.
+    Your goal is to accurately convey the meaning and nuances of the original {from_language} text while adhering to {to_language} grammar, vocabulary, and cultural sensitivities.
+    Produce only the {to_language} translation, without any additional explanations or commentary.
+    Please translate the following {from_language} text into {to_language}:\n\n{prompt}"""
+
+    translation = llm.invoke(instruction)
+
+    return {
+        "prompt": prompt,
+        "translation": translation.content,
+        "from_lang_code": from_lang,
+        "to_lang_code": to_lang
+    }
