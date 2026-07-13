bdd11fdcd2aa7ca66982f6f1c838ae52ab2ce88a
Mick <themickbb@gmail.com>
2026-07-13 13:34:16 +0200

    Updated with customer cache key builders.

--- src/api/v1/models.py
+++ src/api/v1/models.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, Response
 from fastapi_cache.decorator import cache
 
+from core.cache import cache_key_builder
 from core.logging import get_logger
 from io_models.models import (
     ResponseProviderModelsAll,
@@ -19,8 +20,8 @@ router = APIRouter()
     tags=["Models"],
     response_model=ResponseProviderModelsAll
 )
-@cache(expire=120)
-async def get_all_models():
+@cache(expire=120, key_builder=cache_key_builder)
+async def get_all_models(request: Request, response: Response):
     log.debug("Result cached")
     return {
         "provider": await _get_all_models()
@@ -32,8 +33,8 @@ async def get_all_models():
     tags=["Models", "Chat Completion"],
     response_model=ResponseProviderModelsChatCompletions
 )
-@cache(expire=120)
-async def get_all_models_chat_completion():
+@cache(expire=120, key_builder=cache_key_builder)
+async def get_all_models_chat_completion(request: Request, response: Response):
     log.debug("Result cached")
     return {
         "provider": await _get_all_models()
@@ -45,8 +46,8 @@ async def get_all_models_chat_completion():
     tags=["Models", "Translation"],
     response_model=ResponseProviderModelsTranslation
 )
-@cache(expire=120)
-async def get_all_models_translation():
+@cache(expire=120, key_builder=cache_key_builder)
+async def get_all_models_translation(request: Request, response: Response):
     log.debug("Result cached")
     return {
         "provider": await _get_all_models()
@@ -58,8 +59,8 @@ async def get_all_models_translation():
     tags=["Models", "Vector Embedding"],
     response_model=ResponseProviderModelsVectorEmbedding
 )
-@cache(expire=120)
-async def get_all_models_vector_embedding():
+@cache(expire=120, key_builder=cache_key_builder)
+async def get_all_models_vector_embedding(request: Request, response: Response):
     log.debug("Result cached")
     return {
         "provider": await _get_all_models()
