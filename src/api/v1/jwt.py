669c5680df75d5bc4311b0d3ea8f0e00f18873e7
Mick <themickbb@gmail.com>
2026-07-22 18:20:18 +0200

    Updated dependencies.

--- src/api/v1/jwt.py
+++ src/api/v1/jwt.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Depends
 
+from auth.hmac import verify_hmac_signature
 from io_models.jwt import ResponseJWT, PayloadJWT
-from middleware.jwt_auth import jwt_auth
+from middleware.get_client import get_jwt_path_client
 
 router = APIRouter()
 
@@ -9,9 +10,9 @@ router = APIRouter()
     "/jwt",
     tags=["JWT"],
     response_model=ResponseJWT,
-    dependencies=[Depends(jwt_auth)]
+    dependencies=[Depends(verify_hmac_signature)]
 )
-def post_authentication(payload: PayloadJWT):
+def post_authentication(payload: PayloadJWT, client: str = Depends(get_jwt_path_client)):
     return {
         "jwt": "test-jwt"
     }
