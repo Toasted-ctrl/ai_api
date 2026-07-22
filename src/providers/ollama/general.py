a19bcef0e62617beca2d1cc86ac33b9383be1ca0
Mick <themickbb@gmail.com>
2026-07-22 18:53:50 +0200

    Created is_ollama_model_supported.

--- src/providers/ollama/general.py
+++ src/providers/ollama/general.py
@@ -1,8 +1,12 @@
+from fastapi import HTTPException, status
+from ollama import AsyncClient
 import httpx
 
 from core.config import config
 
 async def get_all_models_ollama(host_url) -> list[str]:
+
+    # TODO: Rework this function to use Ollama's AsyncClient instead.
 
     """Returns a dictionary of all available models on the Ollama server.
     Subdivided by expertise (e.g., chat_completion, translation, vector_embedding)."""
@@ -27,3 +31,20 @@ async def get_all_models_ollama(host_url) -> list[str]:
     all_models['vector_embedding'] = [model for model in models if model in config.VECTOR_EMBEDDING_MODELS]
 
     return all_models
+
+
+async def is_ollama_model_supported(model: str, host: str) -> bool:
+
+    """Confirms if a model is supported on the Ollama server.
+    Raises a HTTPException if the model is not supported."""
+
+    client = AsyncClient(host=host)
+    response = await client.list()
+    models = [m.model for m in response.models]
+    if not model in models:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Model '{model}' not supported. Pull the model on the Ollama server first."
+        )
+
+    return True
