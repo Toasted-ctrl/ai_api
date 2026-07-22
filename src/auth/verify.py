c8e3f3e4efafc291ca31a3f6f556ceca88a4bf29
Mick <themickbb@gmail.com>
2026-07-22 18:21:13 +0200

    Implemented general api key dependency.

--- src/auth/verify.py
+++ src/auth/verify.py
@@ -1,8 +1,10 @@
+from fastapi import Security, HTTPException, status
+from fastapi.security import APIKeyHeader
 from typing import Optional
 
 from core.config import config
 
-def get_application(api_key: str) -> Optional[str]:
+def get_client(api_key: str) -> Optional[str]:
 
     """Retrieves which client belongs to the API Key.
     Returns None if the API Key does not match any client."""
@@ -19,3 +21,18 @@ def require_google_id(client: str) -> bool:
     to retrieve a JWT."""
 
     return config.APPLICATIONS[client].require_google_id
+
+
+api_key_header = APIKeyHeader(name="X-API-Key")
+
+async def get_client_from_key(api_key: str = Security(api_key_header)) -> str:
+
+    # TODO: We'll probably want to host clients in a database later.
+
+    if not api_key or get_client(api_key=api_key) is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Missing or invalid API Key"
+        )
+
+    return get_client(api_key=api_key)
