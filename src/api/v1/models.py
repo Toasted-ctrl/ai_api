fe3ce2ed38c9e7220e1cedd93035d56fb8774308
Mick <themickbb@gmail.com>
2026-07-28 20:52:32 +0200

    Added user verification dependency.

--- src/api/v1/models.py
+++ src/api/v1/models.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, Request, Response
+from fastapi import APIRouter, Request, Response, Depends
 from fastapi_cache.decorator import cache
 
+from auth.user import verify_user, VerifiedUser
 from core.cache import cache_key_builder
 from core.logging import get_logger
 from io_models.models import (
@@ -20,8 +21,13 @@ router = APIRouter()
     tags=["Models"],
     response_model=ResponseProviderModelsAll
 )
-@cache(expire=120, key_builder=cache_key_builder)
-async def get_all_models(request: Request, response: Response):
+@cache(expire=300, key_builder=cache_key_builder)
+async def get_all_models(
+    request: Request,
+    response: Response,
+    user: VerifiedUser = Depends(verify_user)
+) -> ResponseProviderModelsAll:
+    
     log.debug("Result cached")
     return {
         "provider": await _get_all_models()
@@ -33,8 +39,13 @@ async def get_all_models(request: Request, response: Response):
     tags=["Models", "Chat Completion"],
     response_model=ResponseProviderModelsChatCompletions
 )
-@cache(expire=120, key_builder=cache_key_builder)
-async def get_all_models_chat_completion(request: Request, response: Response):
+@cache(expire=300, key_builder=cache_key_builder)
+async def get_all_models_chat_completion(
+    request: Request,
+    response: Response,
+    user: VerifiedUser = Depends(verify_user)
+) -> ResponseProviderModelsChatCompletions:
+    
     log.debug("Result cached")
     return {
         "provider": await _get_all_models()
@@ -46,8 +57,13 @@ async def get_all_models_chat_completion(request: Request, response: Response):
     tags=["Models", "Translation"],
     response_model=ResponseProviderModelsTranslation
 )
-@cache(expire=120, key_builder=cache_key_builder)
-async def get_all_models_translation(request: Request, response: Response):
+@cache(expire=300, key_builder=cache_key_builder)
+async def get_all_models_translation(
+    request: Request,
+    response: Response,
+    user: VerifiedUser = Depends(verify_user)
+) -> ResponseProviderModelsTranslation:
+
     log.debug("Result cached")
     return {
         "provider": await _get_all_models()
@@ -59,8 +75,13 @@ async def get_all_models_translation(request: Request, response: Response):
     tags=["Models", "Vector Embedding"],
     response_model=ResponseProviderModelsVectorEmbedding
 )
-@cache(expire=120, key_builder=cache_key_builder)
-async def get_all_models_vector_embedding(request: Request, response: Response):
+@cache(expire=300, key_builder=cache_key_builder)
+async def get_all_models_vector_embedding(
+    request: Request,
+    response: Response,
+    user: VerifiedUser = Depends(verify_user)
+) -> ResponseProviderModelsVectorEmbedding:
+
     log.debug("Result cached")
     return {
         "provider": await _get_all_models()
--- src/core/cache.py
+++ src/core/cache.py
@@ -1,6 +1,12 @@
 from fastapi import Request, Response
 from fastapi_cache import FastAPICache
 
+from core.config import config
+from core.logging import get_logger
+from security.hmac import hash_hmac
+
+log = get_logger()
+
 def cache_key_builder(
     func,
     namespace: str = "",
@@ -9,10 +15,27 @@ def cache_key_builder(
     *args,
     **kwargs
 ):
-    
-    """Build cache key to store results including the email id of the user.
-    Requires request: Request to be included in the main function call."""
 
-    user_email = request.headers.get("X-User-Email", "anonymous")
+    key = config.BLIND_INDEX_HMAC_KEY
+    
+    """Build cache key to store results including hashed api_key, jwt and path.
+    Requires request: Request to be included in the main function call.
+    Should ONLY be used on non-sensitive GET requests."""
+
+    user = kwargs.get("kwargs", {}).get("user")
+    user_id = hash_hmac(
+        content=str(user.id) if user else "Anonymous",
+        key=key
+    )
+
+    api_key = hash_hmac(content=request.headers.get("X-API-Key", "Not Set"), key=key)
+    path = request.url.path
     prefix = FastAPICache.get_prefix()
-    return f"{prefix}:{func.__name__}:{user_email}"
+
+    cache_key = f"{prefix}:{func.__name__}:{api_key}:{user_id}:{path}"
+
+    log.debug(f"Full cache key: '{cache_key}'")
+
+    # TODO: Now the entries are hashed. Do we also need to hash encrypt the output?
+
+    return cache_key
