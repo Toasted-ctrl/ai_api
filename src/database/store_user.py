4349512636f14c08739f7f210546ae9f09312911
Mick <themickbb@gmail.com>
2026-08-04 19:34:36 +0200

    Implemented get_or_store_user(). Might need to replace the old one.

--- src/database/store_user.py
+++ src/database/store_user.py
@@ -43,3 +43,48 @@ def store_user(
     return StoredUser(
         id=user.id
     )
+
+
+def get_or_store_user(
+    session: Session,
+    person_id: uuid.UUID,
+    api_key_id: uuid.UUID,
+    key_type: str,
+    login_provider: str | None = None,
+    external_id: str | None = None,
+) -> StoredUser:
+
+    if key_type == 'Application' and external_id == None:
+        raise ValueError("Unable to add User, if key_type = 'Application', external_id must not be None")
+
+    stored_user = (
+        session.query(Users)
+        .filter(
+            Users.person_id == person_id,
+            Users.api_key_id == api_key_id,
+            Users.external_id == external_id,
+            Users.login_provider == login_provider
+        )
+        .first()
+    )
+    if stored_user:
+        log.info(f"User already exists, returning existing record: '{stored_user.id}'...")
+        return StoredUser(
+            id=stored_user.id
+        )
+
+    user = Users(
+        person_id=person_id,
+        api_key_id=api_key_id,
+        external_id=external_id,
+        login_provider=login_provider
+    )
+
+    session.add(user)
+    session.flush()
+
+    log.info(f"Added User with id: {user.id}")
+
+    return StoredUser(
+        id=user.id
+    )
