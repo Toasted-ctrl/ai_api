37c607b5c17d004a8b072ebfa16bce3b75814607
Mick <themickbb@gmail.com>
2026-08-05 18:12:42 +0200

    Fixed imports, logging.

--- src/setup/application_user.py
+++ src/setup/application_user.py
@@ -2,8 +2,12 @@ from dataclasses import dataclass
 from sqlalchemy.orm import Session
 import uuid
 
-from database.person import get_or_store_person, StoredPerson
-from database.store_user import get_or_store_user, StoredUser
+from core.logging import get_logger
+from database.person import get_or_store_person, Person
+from database.user import get_or_store_user, User
+
+log = get_logger()
+
 
 @dataclass(frozen=True)
 class VerifiedApplicationUser:
@@ -22,14 +26,18 @@ def get_or_create_application_user(
     session: Session
 ) -> VerifiedApplicationUser:
 
-    person: StoredPerson = get_or_store_person(
+    """Stores or fetches a new frontend Application User."""
+
+    log.debug("Creating or fetching Application User...")
+
+    person: Person = get_or_store_person(
         session=session,
         first_name=first_name,
         last_name=last_name,
         email=email
     )
 
-    user: StoredUser = get_or_store_user(
+    user: User = get_or_store_user(
         session=session,
         person_id=person.id,
         api_key_id=client_id,
@@ -37,6 +45,8 @@ def get_or_create_application_user(
         login_provider=login_provider,
         external_id=external_id
     )
+
+    log.debug("Returning Application User...")
 
     return VerifiedApplicationUser(
         client_id=client_id,
