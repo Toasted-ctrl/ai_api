a9f58f76b162ad1a6400b60326e06278d9268834
Mick <themickbb@gmail.com>
2026-08-07 15:02:16 +0200

    Initial create.

---
+++ src/providers/openai/models.py
@@ -0,0 +1,34 @@
+from openai import OpenAI
+
+from core.logging import get_logger
+
+log = get_logger()
+
+
+# TODO: Write test.
+
+def get_models(
+    api_key: str,
+    base_url: str
+) -> dict[str, list[str]]:
+
+    """Fetches and returns all models supported by the API Key.
+    This method will work for both OpenAI as well as Melious endpoints."""
+
+    client = OpenAI(
+        api_key=api_key,
+        base_url=base_url
+    )
+
+    models = client.models.list()
+
+    log.debug(f"Fetching models for Provider URL '{base_url}' with API Key '{api_key[:10]}'...")
+
+    # TODO: Likely build in some separation here, some models might be
+    # Vector Embedding models.
+    
+    return {
+        "chat_completion": [model.id for model in models.data],
+        "translation": [],
+        "vector_embedding": []
+    }
