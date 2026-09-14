ffb00f7030fbb7c0f29fc072b3a3577c0f204580
Mick <themickbb@gmail.com>
2026-09-14 18:13:11 +0200

    Simplified /providers/models endpoint.

--- src/api/v1/models.py
+++ src/api/v1/models.py
@@ -13,130 +13,41 @@ from auth.dep_verify_user import depends_verify_user, VerifiedUser
 from core.cache import cache_key_builder
 from core.logging import get_logger
 from database.session import get_db_session
-from iom.models import (
-    ResponseProviderModelsAll,
-    ResponseProviderModelsChatCompletions,
-    ResponseProviderModelsTranslation,
-    ResponseProviderModelsVectorEmbedding
-)
+from iom.models import ResponseProviderModels
 from providers.models import get_all_models
 
+
 log = get_logger()
+
 
 router = APIRouter()
 
 
 @router.get(
-    "/models",
-    tags=["Models"],
-    response_model=ResponseProviderModelsAll
+    "/providers/models",
+    tags=["Providers"],
+    response_model=ResponseProviderModels,
+    description="Returns a list of available models, divided by Provider and model expertise."
 )
 @cache(expire=300, key_builder=cache_key_builder)
-async def get_all(
+async def get_models(
     request: Request,
     response: Response,
     user: VerifiedUser = Depends(depends_verify_user),
     session: Session = Depends(get_db_session)
-) -> ResponseProviderModelsAll:
+) -> ResponseProviderModels:
 
-    provider_models = await get_all_models(
+    pm = await get_all_models(
         session=session,
         user_id=user.id
     )
 
-    if provider_models == {}:
+    if pm == {}:
         raise HTTPException(
             status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
             detail="No models available"
         )
-    
-    return {
-        "providers": provider_models
-    }
 
-
-@router.get(
-    "/models/chat_completion",
-    tags=["Models", "Chat Completion"],
-    response_model=ResponseProviderModelsChatCompletions
-)
-@cache(expire=300, key_builder=cache_key_builder)
-async def get_all_chat_completion(
-    request: Request,
-    response: Response,
-    user: VerifiedUser = Depends(depends_verify_user),
-    session: Session = Depends(get_db_session)
-) -> ResponseProviderModelsChatCompletions:
-    
-    provider_models = await get_all_models(
-        session=session,
-        user_id=user.id
+    return ResponseProviderModels(
+        providers=pm
     )
-
-    if provider_models == {}:
-        raise HTTPException(
-            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
-            detail="No models available"
-        )
-        
-    return {
-        "providers": provider_models
-    }
-
-
-@router.get(
-    "/models/translation",
-    tags=["Models", "Translation"],
-    response_model=ResponseProviderModelsTranslation
-)
-@cache(expire=300, key_builder=cache_key_builder)
-async def get_all_translation(
-    request: Request,
-    response: Response,
-    user: VerifiedUser = Depends(depends_verify_user),
-    session: Session = Depends(get_db_session)
-) -> ResponseProviderModelsTranslation:
-
-    provider_models = await get_all_models(
-        session=session,
-        user_id=user.id
-    )
-
-    if provider_models == {}:
-        raise HTTPException(
-            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
-            detail="No models available"
-        )
-        
-    return {
-        "providers": provider_models
-    }
-
-
-@router.get(
-    "/models/vector_embedding",
-    tags=["Models", "Vector Embedding"],
-    response_model=ResponseProviderModelsVectorEmbedding
-)
-@cache(expire=300, key_builder=cache_key_builder)
-async def get_all_vector_embedding(
-    request: Request,
-    response: Response,
-    user: VerifiedUser = Depends(depends_verify_user),
-    session: Session = Depends(get_db_session)
-) -> ResponseProviderModelsVectorEmbedding:
-
-    provider_models = await get_all_models(
-        session=session,
-        user_id=user.id
-    )
-
-    if provider_models == {}:
-        raise HTTPException(
-            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
-            detail="No models available"
-        )
-        
-    return {
-        "providers": provider_models
-    }
--- src/iom/models.py
+++ src/iom/models.py
@@ -1,34 +1,5 @@
 from pydantic import BaseModel
 
-class ProviderModelsAll(BaseModel):
-    chat_completion: list[str]
-    translation: list[str]
-    vector_embedding: list[str]
 
-
-class ResponseProviderModelsAll(BaseModel):
-    providers: dict[str, ProviderModelsAll]
-
-
-class ProviderModelsChatCompletions(BaseModel):
-    chat_completion: list[str]
-
-
-class ResponseProviderModelsChatCompletions(BaseModel):
-    providers: dict[str, ProviderModelsChatCompletions]
-
-
-class ProviderModelsTranslations(BaseModel):
-    translation: list[str]
-
-
-class ResponseProviderModelsTranslation(BaseModel):
-    providers: dict[str, ProviderModelsTranslations]
-
-
-class ProviderModelsVectorEmbedding(BaseModel):
-    vector_embedding: list[str]
-
-
-class ResponseProviderModelsVectorEmbedding(BaseModel):
-    providers: dict[str, ProviderModelsVectorEmbedding]
+class ResponseProviderModels(BaseModel):
+    providers: dict[str, dict[str, list[str]]]
